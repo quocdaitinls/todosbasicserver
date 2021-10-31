d7593efe07ff2904fe64372ca4d490869becf5d2
QUOC DAI <quocdaitinls@gmail.com>
2021-10-31 14:03:18 +0700

    fix cookie option

--- src/routes/auth/utils/cookieOptions.js
+++ src/routes/auth/utils/cookieOptions.js
@@ -1,7 +1,7 @@
 export const cookieOptions = (req, time = 900) => ({
-    domain: req.get("origin").replace("https://", ""),
+    domain: req.get("origin"),
     sameSite: "none",
     secure: true,
-    httpOnly: true,
+    // httpOnly: true,
     expires: new Date(Date.now() + time * 1000),
 });
